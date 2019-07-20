e5444caf5d71b0276c7f1373c9f238ad66df2d3c
DEADB17 <DEADB17@gmail.com>
2019-07-20 09:20:28 -0400

    Always quote props in the config

--- conf/strict.js
+++ conf/strict.js
@@ -6,7 +6,7 @@
 
 "use strict";
 
-/* eslint sort-keys: ["error", "asc"] */
+/* eslint sort-keys: ["error", "asc"], quote-props: ["error", "always"] */
 
 module.exports = {
     "accessor-pairs": "error",
